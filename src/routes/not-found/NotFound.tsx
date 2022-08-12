d1e47174b4fd35d784ba62f8dd5dd03e60036aa7
Camila Hatakeyama <hatakeyama.camila@gmail.com@>
2022-08-12 16:29:22 -0300

    feat: ajusta rota not-found

--- src/routes/not-found/NotFound.tsx
+++ src/routes/not-found/NotFound.tsx
@@ -1,4 +1,20 @@
+import React from "react"
+import Container from "@mui/material/Container"
 
 export default function NotFound() {
-    return 'Not found'
+  return (
+    <>
+      <Container>
+        <main
+          style={{
+            marginTop: "4rem",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          Página não encontrada
+        </main>
+      </Container>
+    </>
+  )
 }
