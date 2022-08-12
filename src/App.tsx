169c21638858f1a56d9113f97ae3d318b0708733
Camila Hatakeyama <hatakeyama.camila@gmail.com@>
2022-08-12 15:49:25 -0300

    refactor: remove ponto e vírgula, melhora nome de variável, adiciona tipo do useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import useDebounce from "./hooks/useDebounce.js";
-import { search, getAll } from "./services/MenuService.tsx";
-import SearchBar from "./components/SearchBar/SearchBar.tsx";
-import { OrderProvider } from "./contexts/OrderContext.tsx";
-import Menu from "./routes/menu/Menu.tsx";
-import About from "./routes/about/About.tsx";
-import NotFound from "./routes/not-found/NotFound.tsx";
-import "./App.scss";
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react"
+import useDebounce from "./hooks/useDebounce.js"
+import { search, getAll } from "./services/MenuService.tsx"
+import SearchBar from "./components/SearchBar/SearchBar.tsx"
+import { OrderProvider } from "./contexts/OrderContext.tsx"
+import Menu from "./routes/menu/Menu.tsx"
+import About from "./routes/about/About.tsx"
+import NotFound from "./routes/not-found/NotFound.tsx"
+import "./App.scss"
+import MenuItem from "./interfaces/MenuItem.js"
 
 export default function App() {
-  const [menuItem, setMenuItem] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [menuItems, setMenuItem] = useState<MenuItem[]>([])
+  const [searchTerm, setSearchTerm] = useState("")
 
-  const debounceSearchTerm = useDebounce(searchTerm, 1000);
-
-  useEffect(() => {
-    loadMenuItems();
-  }, []);
+  const debounceSearchTerm = useDebounce(searchTerm, 1000)
 
   useEffect(() => {
-    searchMenuItem(debounceSearchTerm);
-  }, [debounceSearchTerm]);
+    loadMenuItems()
+  }, [])
+
+  useEffect(() => {
+    searchMenuItem(debounceSearchTerm)
+  }, [debounceSearchTerm])
 
   const loadMenuItems = async () => {
-    const allMenu = await getAll();
-    setMenuItem(allMenu);
-  };
+    const allMenu = await getAll()
+    setMenuItem(allMenu)
+  }
 
   const searchMenuItem = async (text: string) => {
-    const { data } = await search(text);
-    setMenuItem(data);
-  };
+    const { data } = await search(text)
+    setMenuItem(data)
+  }
 
   return (
     <>
@@ -41,7 +42,7 @@ export default function App() {
           <SearchBar handleOnChange={(e) => setSearchTerm(e.target.value)} />
           <div className="outlet-content">
             <Routes>
-              <Route path="/" element={<Menu menuItems={menuItem} />} />
+              <Route path="/" element={<Menu menuItems={menuItems} />} />
               <Route path="/about" element={<About />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
@@ -49,5 +50,5 @@ export default function App() {
         </OrderProvider>
       }
     </>
-  );
+  )
 }
