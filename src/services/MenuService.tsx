86c95b9555dc952a8ae71f3b5b99a5d036ae9605
Camila Hatakeyama <hatakeyama.camila@gmail.com@>
2022-08-12 15:46:51 -0300

    refactor: remove ponto e vírgula e adiciona tipo do retorno de função

--- src/services/MenuService.tsx
+++ src/services/MenuService.tsx
@@ -1,22 +1,22 @@
-import axios from "axios";
-import config from "../config.json";
-import MenuItem from "../interfaces/MenuItem";
-import Category from "../interfaces/Category";
+import axios from "axios"
+import config from "../config.json"
+import MenuItem from "../interfaces/MenuItem"
+import Category from "../interfaces/Category"
 
-const apiEndpoint = config.apiUrl;
+const apiEndpoint = config.apiUrl
 
 export async function getAll(): Promise<MenuItem[]> {
-  const promise = axios.get(`${apiEndpoint}/menuItems`);
-  const { data } = await promise;
-  return data;
+  const promise = axios.get(`${apiEndpoint}/menuItems`)
+  const { data } = await promise
+  return data
 }
 
 export async function getCategories(): Promise<Category[]> {
-  const promise = axios.get(`${apiEndpoint}/categories`);
-  const { data } = await promise;
-  return data;
+  const promise = axios.get(`${apiEndpoint}/categories`)
+  const { data } = await promise
+  return data
 }
 
-export function search(text: string) {
-  return axios.get(`${apiEndpoint}/menuItems?title_like=${text}`);
+export function search(text: string): Promise<MenuItem[]> {
+  return axios.get(`${apiEndpoint}/menuItems?title_like=${text}`)
 }
