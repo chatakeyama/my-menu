d4d82366f6ba1a598cf75904d588863b50ace38e
Camila Hatakeyama <hatakeyama.camila@gmail.com@>
2022-08-15 13:33:34 -0300

    feat: adiciona tratamento para caso em que não consiga retornar as categorias

--- src/components/TabsNavBar/TabsNavBar.tsx
+++ src/components/TabsNavBar/TabsNavBar.tsx
@@ -6,14 +6,19 @@ import "./TabsNavBar.scss"
 const TabsNavBar = () => {
   const [selectedCategory, setSelectedCategory] = useState(1)
   const [categories, setCategories] = useState<Category[]>([])
+  const [httpError, setHttpError] = useState(false)
 
   useEffect(() => {
     loadCategories()
   }, [])
 
   const loadCategories = async () => {
-    const categories = await getCategories()
-    setCategories(categories)
+    try {
+      const categories = await getCategories()
+      setCategories(categories)
+    } catch (exception) {
+      setHttpError(true)
+    }
   }
 
   const handleChange = (categoryId: number): void => {
@@ -22,27 +27,30 @@ const TabsNavBar = () => {
 
   return (
     <>
-      <div className="navbar">
-        <div className="navbar__list">
-          {categories.map((category: Category) => {
-            return (
-              <div className="navbar__item" key={category.id}>
-                <a
-                  onClick={() => handleChange(category.id)}
-                  className={
-                    selectedCategory == category.id
-                      ? "navbar__anchor active"
-                      : "navbar__anchor"
-                  }
-                  href={`#${category.id}`}
-                >
-                  {category.name}
-                </a>
-              </div>
-            )
-          })}
+      {!httpError && (
+        <div className="navbar">
+          <div className="navbar__list">
+            {categories &&
+              categories.map((category: Category) => {
+                return (
+                  <div className="navbar__item" key={category.id}>
+                    <a
+                      onClick={() => handleChange(category.id)}
+                      className={
+                        selectedCategory == category.id
+                          ? "navbar__anchor active"
+                          : "navbar__anchor"
+                      }
+                      href={`#${category.id}`}
+                    >
+                      {category.name}
+                    </a>
+                  </div>
+                )
+              })}
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
